cf481473a999564f8ff33c57a85c2609d061fa6a
kluedskincare <kluedxposproject@gmail.com>
2024-02-22 13:02:35 +0800

    version 001.037

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -156,7 +156,7 @@ function Register() {
                                     </div>
                                 </div>
                                 <button disabled={
-                                    password!=="" && password.length>=6 && password.length>16 && checkPassword!=="" && password===checkPassword && checked===true ? 
+                                    password!=="" && password.length>=6 && password.length<16 && checkPassword!=="" && password===checkPassword && checked===true ? 
                                     false : true
                                 } type="submit" className={`w-full text-white ${password!=="" && password.length>=6 && checkPassword!=="" && password===checkPassword && checked===true ? 'hover:bg-blue-600 bg-blue-500' : 'bg-gray-500'} focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}>Create an account</button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
